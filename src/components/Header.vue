648dad45cb0f8448c2197f8251b3793369955edb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-03-20 17:11:57 +0100

    dev: use guard instead of handleNavigation specific function

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,7 @@
 <template>
   <header class="bg-blue-600 p-4 text-white">
     <nav class="container mx-auto flex justify-center">
-      <a
-        href="#"
-        class="text-2xl font-bold hover:underline"
-        @click="handleNavigation"
-      >
-        Quizify
-      </a>
+      <a href="#" class="text-2xl font-bold hover:underline"> Quizify </a>
     </nav>
   </header>
 </template>
-
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { useQuizStore } from '../stores/quizStore'
-
-const router = useRouter()
-const quizStore = useQuizStore()
-
-const handleNavigation = (event: MouseEvent) => {
-  if (quizStore.quizEnCours) {
-    const confirmation = confirm(
-      'Vous êtes en train de faire un quiz. Voulez-vous vraiment quitter ?',
-    )
-    if (!confirmation) {
-      event.preventDefault()
-      return
-    }
-    quizStore.terminerQuiz()
-  }
-  router.push('/')
-}
-</script>
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { useQuizStore } from './stores/quizStore'
 import Home from './views/Home.vue'
 import Quiz from './views/Quiz.vue'
 import Results from './views/Results.vue'
@@ -15,3 +16,16 @@ const router = createRouter({
 })
 
 export default router
+
+router.beforeEach(() => {
+  const quizStore = useQuizStore()
+  if (
+    quizStore.quizEnCours &&
+    !confirm(
+      'Vous êtes en train de faire un quiz. Voulez-vous vraiment quitter ?',
+    )
+  ) {
+    return false // Bloque la navigation
+  }
+  return true // Continue la navigation
+})
